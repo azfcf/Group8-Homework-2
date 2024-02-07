c67d9538f43371c4f90bef78dad0d16df91377f7
RedPh <lmb7gq@umsystem.edu>
2024-02-07 16:58:51 -0600

    Added the  updatStudentGradeById function that updates a students Graded

--- Homework2/src/edu/mu/studentManager/StudentManager.java
+++ Homework2/src/edu/mu/studentManager/StudentManager.java
@@ -25,4 +25,29 @@ public class StudentManager {
             }
         }
     }
+	
+	public boolean updateStudentGradeById(int id, double grade) { 
+		if ( student == null || students.length == 0) {// test to see if student exsist or if students array has a student in it at all 
+			System.out.println("No Student to Update.");
+			return false;
+		}
+		
+		boolean studentDoesExist = searchStudentById(id);// call  searchStudent function to check if the id that was inserted matches with the id of a student in the array 
+		if (studentDoesExist) {// if student does exist then go to change grade 
+			
+			 for (Student student : students) {// loop through students 
+				 if (student.getId()==id) {// check to make sure ID match 
+				 student.setGrade(grade);// change grade 
+				 System.out.println("Student Grade updated");
+				 return true;
+				 }
+			 }
+		}
+		else {
+			System.out.println("Can not find a student with ID" + id + " please insert working Id #")// failes to find student with id 
+		}
+		return false ;
+	}
+
+
 }
