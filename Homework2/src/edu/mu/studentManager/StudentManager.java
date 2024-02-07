a12250e493794512eb8fcd6007d5f49e3cf04af2
Ryan Esparza <Ryan Esparza@DESKTOP-MVS95Q2>
2024-02-07 16:27:01 -0600

    Implemented displayStudents() method to show the details of all the students

--- Homework2/src/edu/mu/studentManager/StudentManager.java
+++ Homework2/src/edu/mu/studentManager/StudentManager.java
@@ -5,4 +5,14 @@ import edu.mu.student.Student;
 public class StudentManager {
 	private Student[] students;
 
+	public void displayStudents() {
+        if (students == null || students.length == 0) {
+            System.out.println("No students to display");
+        } else {
+            System.out.println("Details of Students:");
+            for (Student student : students) {
+                System.out.println(student.toString());
+            }
+        }
+    }
 }
