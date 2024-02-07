b625c099b1790379f321083858783657dc54957f
azfcf <azfcf@umsystem.edu>
2024-02-07 16:31:35 -0600

    Add student search by ID method

--- Homework2/src/edu/mu/studentManager/StudentManager.java
+++ Homework2/src/edu/mu/studentManager/StudentManager.java
@@ -4,5 +4,15 @@ import edu.mu.student.Student;
 
 public class StudentManager {
 	private Student[] students;
+	
+	public boolean searchStudentById(int id) {
+		for(Student student : students) {
+			if(student.getId() == id) {
+				System.out.println(student.toString());
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
